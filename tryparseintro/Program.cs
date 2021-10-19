8c53c68c69dfc9a64d02474acaaf9f4920637347
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2021-10-19 11:08:39 +0200

    Transaction

---
+++ tryparseintro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+
+
+System.Console.WriteLine("You enter the Horseshoe Pawnshop, what would you like?");
+
+Console.ReadLine();
+
+System.Console.WriteLine("List of items:\n A:\n B:\n C:\n D:\n E:\n F:\n G:\n H:\n I:\n J:\n K:\n L:");
+System.Console.WriteLine("What would you like?");
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

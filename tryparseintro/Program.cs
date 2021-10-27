6c8482132ce01d301434d6c0625abd9f4d009040
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2021-10-27 11:05:23 +0200

    slightprogress

--- tryparseintro/Program.cs
+++ tryparseintro/Program.cs
@@ -31,14 +31,42 @@ while (descision != "a" && descision != "b" && descision != "c")
     System.Console.WriteLine("What would you like?");
     descision = Console.ReadLine();
     descision = descision.ToLower();
+
+
+
+
+
+
+    if (descision == "c") {
+System.Console.WriteLine("You want to buy Malgo Balls Club, why?");
+Console.ReadLine();
+
+    }
+
+if (descision == "b") {
+System.Console.WriteLine("You want to buy a rubber horse, how many?");
+Console.ReadLine();
+
+}
+
+if (descision == "a") {
+System.Console.WriteLine("You want to buy a rubber duck, how many?");
+Console.ReadLine();
+
+}
+
 }
 
 // int antal
+int amount = 0;
 
 while (transaction == false)
 {
 
 
+
+
+System.Console.WriteLine($"you now have {Credit} credits");
 
 
 
@@ -52,27 +80,6 @@ while (transaction == false)
 
 
 // fixa while ( transaction == false), programmet verkar förstört för att när jag lade in dialog i if(c) så hände inget när man kom till den delen, fixa det, kom ihåg att kolla på https://github.com/NTISTHLM-TESPEL-PRR01-2122/TE20B_TryParse/blob/master/TE20B_TryParse/Program.cs.
-if (descision == "a")
-{
-
-
-}
-
-if (descision == "b")
-{
-
-
-}
-
-if (descision == "c")
-{
-
-
-}
-
-
-
-
 
 
 
